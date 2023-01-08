03130646cd3f22b2e31c1532a7fb0616a825b49f
Michael Matheus <michaelmatheus2001@gmail.com>
2023-01-08 07:25:03 -0300

    dealing test API fetched data

--- src/components/GetApiData/index.tsx
+++ src/components/GetApiData/index.tsx
@@ -3,8 +3,14 @@ import { useState } from "react"
 import './styles.css';
 
 
+
+type Movie = {
+    titulo: string;
+    avatar: string;
+}
+
 export function GetApiData() {
-    const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState<Movie[]>([]);
 
     function handleGetMoviesData() {
         fetch('https://api.b7web.com.br/cinema/')
@@ -13,9 +19,17 @@ export function GetApiData() {
     }
 
     return (
-        <div>
+        <div className="movies">
             <button onClick={handleGetMoviesData}>Get Movies</button>
             <div>Total movies: {movies.length}</div>
+            <div className="movies-grid">
+                {movies.map((movie, index) => (
+                    <div key={index} className="movie-grid-item">
+                        <img src={movie.avatar} alt="movie avatar" />
+                        <p>{movie.titulo}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
