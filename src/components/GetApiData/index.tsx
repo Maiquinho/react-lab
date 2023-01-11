7bcfd3badcb894d53583e7141a0b2a49ad78560c
Michael Matheus <michaelmatheus2001@gmail.com>
2023-01-11 05:42:54 -0300

    test loading state & disabled

--- src/components/GetApiData/index.tsx
+++ src/components/GetApiData/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useState } from "react";
+import { Loading } from "../Loading";
 
 import './styles.css';
 
@@ -10,28 +11,39 @@ type Movie = {
 }
 
 export function GetApiData() {
+    const [loading, setLoading] = useState(false);
     const [movies, setMovies] = useState<Movie[]>([]);
 
     async function handleGetMoviesData() {
+        setLoading(true);
         const response = await fetch('https://api.b7web.com.br/cinema/');
         const data = await response.json();
-        
+        setLoading(false);
+
         setMovies(data);
     }
 
     return (
-        <div className="movies">
-            <div className="movies-grid">
-                {movies.map((movie, index) => (
-                    <div key={index} className="movie-grid-item">
-                        <img src={movie.avatar} alt="movie avatar" />
-                        <p>{movie.titulo}</p>
+        <>
+            {loading ? (
+                <Loading />
+            ) : 
+            (
+                <div className="movies">
+                    <div className="movies-grid">
+                        {movies.map((movie, index) => (
+                            <div key={index} className="movie-grid-item">
+                                <img src={movie.avatar} alt="movie avatar" />
+                                <p>{movie.titulo}</p>
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
 
-            <button onClick={handleGetMoviesData}>Get Movies</button>
-            <div>Total movies: {movies.length}</div>
-        </div>
+                    <button onClick={handleGetMoviesData} disabled={movies.length != 0 ? true : false}>Get Movies</button>
+                    <div>Total movies: {movies.length}</div>
+                </div>
+            )
+            }
+        </>
     )
 }
