45e94f19ce94fe8d2ce08b567a9c72d2e1b446f6
Michael Matheus <michaelmatheus2001@gmail.com>
2023-04-06 07:05:33 -0300

    feat: conditional rendering (post component)

--- src/components/PostsComponent/index.tsx
+++ src/components/PostsComponent/index.tsx
@@ -1,28 +1,61 @@
 import { useEffect, useState } from 'react';
+import { Loading } from '../Loading';
 import { PostComponent } from '../PostComponent';
 import './styles.css';
 import { Post } from '../../types/Post';
 
 
 export function PostsComponent() {
+    const [loading, setLoading] = useState(true);
     const [posts, setPosts] = useState<Post[]>([]);
 
     useEffect(() => {
         handlePosts();
     })
 
-    async function handlePosts(){
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-        const json = await response.json();
+    async function handlePosts() {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+            const json = await response.json();
 
-        setPosts(json);
+            setLoading(false);
+            setPosts(json);
+        } catch (error) {
+            setLoading(false);
+            console.log(error);
+        }
     }
 
     return (
-        <article className="posts-article">
-            <h2>Posts</h2>
+        <>
+            {loading ? <Loading /> :
 
-            {posts.map((post, i) => <PostComponent key={i} {...post} />) }
-        </article>
+                (
+                    <>
+                        {posts.length > 0 ? (
+
+                            <article className="posts-article">
+                                <div className="posts-article__title">
+                                    <h2>Posts</h2>
+                                    <small>Total: {posts.length}</small>
+                                </div>
+
+                                {posts.map((post, i) => <PostComponent key={i} {...post} />)}
+                            </article>
+
+                        ) : (
+
+                            <article className="posts-article">
+                                <div className="posts-article__title">
+                                    <h2>Você não tem posts</h2>
+                                </div>
+                            </article>
+
+                        )}
+                    </>
+                )
+                
+            }
+        </>
     )
 }
