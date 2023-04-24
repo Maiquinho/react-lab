47a2988830343d5943609c89c24cbfccc1a1b1f2
Michael Matheus <michaelmatheus2001@gmail.com>
2023-04-24 17:18:10 -0300

    feat: employee list cards

--- src/components/EmployeeList/index.tsx
+++ src/components/EmployeeList/index.tsx
@@ -1,5 +1,6 @@
 import { useEmployeeReducer } from '../../hooks/reducers/employeeReducer';
 import { AddEmployee } from '../AddEmployee';
+import { Trash, SortDescending } from '@phosphor-icons/react';
 
 import './styles.css';
 
@@ -10,22 +11,47 @@ export function EmployeeList() {
 
     return (
         <div className='employee-list'>
-            <h2>Lista de Funcionários</h2>
-
-            <ul>
-                {employeeListState.map((employeeListItem, index) => (
-                    <li key={index}>id: {employeeListItem.id} Nome: {employeeListItem.name}</li>
-                ))}
-            </ul>
-
-            <AddEmployee 
-                onAdd={newEmployee => employeeListDispatch({ 
-                    type: 'ADD', 
-                    payload: { 
-                        name: newEmployee 
-                    } 
-                })} 
+            <AddEmployee
+                onAdd={newEmployee => employeeListDispatch({
+                    type: 'ADD',
+                    payload: {
+                        name: newEmployee
+                    }
+                })}
             />
+
+
+
+            {employeeListState.length > 0 &&
+
+                <section className='employee-section'>
+
+                    <div className='employee-title'>
+                        <h2>Lista de Funcionários</h2>
+                        {employeeListState.length > 1 ? 
+                            <SortDescending size={24} weight="bold" onClick={() => employeeListDispatch({ type: 'SORT' })} /> : <SortDescending size={24} weight="bold" color='#929292de' />
+                        }
+                    </div>
+
+
+                    {employeeListState.map((employeeListItem, index) => (
+
+                        <div className='employee-card' key={index}>
+                            <div className='employee-card__info'>
+                                <h3>{employeeListItem.name}</h3>
+                                <small>id: {employeeListItem.id}</small>
+                            </div>
+                            <button onClick={() => employeeListDispatch({ type: 'DEL', payload: { id: employeeListItem.id } })}>
+                                <Trash size={16} color="#d20000" weight="bold" />
+                            </button>
+                        </div>
+
+                    ))}
+
+                </section>
+
+            }
+
 
         </div>
     )
