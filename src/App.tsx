7568dfe5e2188bea8160c5bfade08cc7ff305ab1
Michael Matheus <michaelmatheus2001@gmail.com>
2023-06-09 06:26:41 -0300

    build: add error page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { AboutMichael } from './pages/AboutMichael';
 import { AboutReactLab } from './pages/AboutReactLab';
+import { NotFound } from './pages/NotFound';
 import { Faq } from './pages/Faq';
 
 import { Routes, Route } from 'react-router-dom';
@@ -23,6 +24,7 @@ export default function App() {
         <Route path="/about/michael" element={<AboutMichael />} />
         <Route path="/about/reactlab" element={<AboutReactLab />} />
         <Route path="/faq" element={<Faq />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
 
       <Footer />
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+export function NotFound(){
+
+    return (
+        <div>
+            <h1>Página não encontrada!</h1>
+
+            <p>A página que você está procurando não existe ou foi removida.</p>
+        </div>
+    )
+}
