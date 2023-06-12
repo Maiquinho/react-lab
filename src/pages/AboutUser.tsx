196b93b5a5f20a936fedf5ced09f43a982cdf304
Michael Matheus <michaelmatheus2001@gmail.com>
2023-06-12 06:24:04 -0300

    build: add react router navigate on about page

--- src/pages/AboutUser.tsx
+++ src/pages/AboutUser.tsx
@@ -1,13 +1,33 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 
 export function AboutUser(){
     const params = useParams();
     const userName = (params.slug) ? params.slug.charAt(0).toUpperCase() + params.slug.substring(1) : null;
+    const navigate = useNavigate();
+
+
+    function backToHome(){
+        navigate('/');
+    }
+
+    function backToAbout(){
+        navigate('/about');
+    }
+
 
     return (
         <div>
             <h1>Sobre: {userName}</h1>
+            <ul>
+                <li>
+                    <div onClick={backToHome}>Home</div>
+                </li>
+                <li>
+                    <div onClick={backToAbout}>Sobre</div>
+                </li>
+            </ul>
+
 
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam voluptatum vero, eveniet molestias perferendis iste rerum expedita culpa architecto sit ab recusandae, iusto magnam aspernatur labore debitis adipisci, libero sunt!</p>
         </div>
